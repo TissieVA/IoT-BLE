4e3e125c66c2e379431fa7a12ddbf29e5f2ed70a
Tijs Van Alphen <tijs.van.alphen@hotmail.com>
2022-01-25 18:34:30 +0100

    Removed unused files

--- nRF5_SDK_15.0.0_a53641a/examples/ble_peripheral/custom_ble_service_example/pca10040/s132/ses/Output/Debug/Obj/ble_app_template_pca10040_s132/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/Debug/Obj/ble_app_template_pca10040_s132/sensorsim.o: \
- C:\nRF5\nRF5_SDK_15.0.0_a53641a\components\libraries\sensorsim\sensorsim.c \
- C:\nRF5\nRF5_SDK_15.0.0_a53641a\components\libraries\sensorsim\sensorsim.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.64/include/stdbool.h
